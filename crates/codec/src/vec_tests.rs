19a14d451634f09a94a9a8f42e3919f4afc4f886
Stas Stepanov <stanislav@ankr.com>
2024-04-15 15:42:39 +0600

    codec: empty vec

--- crates/codec/src/vec_tests.rs
+++ crates/codec/src/vec_tests.rs
@@ -332,3 +332,81 @@ fn test_field_encoder_be_a32_vec_u16() {
     v1_in.resize(v1_in_len_aligned, 0);
     assert_eq!(v1_in, v1_out);
 }
+
+#[test]
+fn test_field_encoder_be_ad_vec_u16_empty() {
+    type End = BE;
+    const ALIGN: usize = ALIGN_DEFAULT;
+    type V1ItemType = u16;
+    type V1Type = Vec<V1ItemType>;
+    let mut v1_in: V1Type = Vec::from([]);
+    let offset = 0;
+    let header_item_size = header_item_size!(ALIGN);
+    let header_size = header_size!(ALIGN, 3);
+    assert_eq!(
+        header_size,
+        field_encoder_const_val!(V1Type, End, ALIGN, HEADER_SIZE)
+    );
+    let mut buffer = DynamicBuffer::<End>::new(header_size, None);
+    <V1Type as FieldEncoder<End, ALIGN, V1Type>>::encode(&v1_in, &mut buffer, offset);
+    let encoded_value = buffer.finalize();
+    for (i, v) in encoded_value
+        .as_slice()
+        .chunks(header_item_size)
+        .enumerate()
+    {
+        let chunk_encoded = hex::encode(v);
+        println!("fact chunk {i}: {chunk_encoded}")
+    }
+    let expected = "\
+    00000000\
+    0000000c\
+    00000000\
+    ";
+    let fact = hex::encode(&encoded_value);
+    assert_eq!(expected, fact);
+    let buffer = ReadableBuffer::<End>::new(&encoded_value);
+    let mut v1_out: V1Type = Vec::new();
+    <V1Type as FieldEncoder<End, ALIGN, V1Type>>::decode(&buffer, offset, &mut v1_out);
+    assert_eq!(v1_in, v1_out);
+}
+
+#[test]
+fn test_field_encoder_be_a32_vec_u16_empty() {
+    type End = BE;
+    const ALIGN: usize = ALIGN_32;
+    type V1ItemType = u16;
+    type V1Type = Vec<V1ItemType>;
+    let mut v1_in: V1Type = Vec::from([]);
+    let offset = 0;
+    let header_item_size = header_item_size!(ALIGN);
+    let header_size = header_size!(ALIGN, 3);
+    assert_eq!(
+        header_size,
+        field_encoder_const_val!(V1Type, End, ALIGN, HEADER_SIZE)
+    );
+    let mut buffer = DynamicBuffer::<End>::new(header_size, None);
+    <V1Type as FieldEncoder<End, ALIGN, V1Type>>::encode(&v1_in, &mut buffer, offset);
+    let encoded_value = buffer.finalize();
+    for (i, v) in encoded_value
+        .as_slice()
+        .chunks(header_item_size)
+        .enumerate()
+    {
+        let chunk_encoded = hex::encode(v);
+        println!("fact chunk {i}: {chunk_encoded}")
+    }
+    let expected = "\
+    0000000000000000000000000000000000000000000000000000000000000000\
+    0000000000000000000000000000000000000000000000000000000000000060\
+    0000000000000000000000000000000000000000000000000000000000000000\
+    ";
+    let fact = hex::encode(&encoded_value);
+    assert_eq!(expected, fact);
+    let buffer = ReadableBuffer::<End>::new(&encoded_value);
+    let mut v1_out: V1Type = Vec::new();
+    <V1Type as FieldEncoder<End, ALIGN, V1Type>>::decode(&buffer, offset, &mut v1_out);
+    let v1_in_len_aligned = dynamic_size_aligned!(ALIGN, v1_in.len(), V1ItemType);
+    v1_in.resize(v1_in_len_aligned, 0);
+    assert_eq!(v1_in, v1_out);
+}
